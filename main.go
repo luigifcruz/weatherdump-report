8eab302d1e84e53ee1277e98c0e7fb2dc1c62e20
Luigi Cruz <luigifcruz@icloud.com>
2019-04-09 19:06:41 -0300

    go fmt files

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"io/ioutil"
-	"encoding/json"
-	"github.com/fatih/color"
-	"github.com/gorilla/mux"
-	"github.com/google/uuid"
-	"github.com/gorilla/schema"
 )
 
 var decoder = schema.NewDecoder()
@@ -18,20 +18,20 @@ func main() {
 	color.Green("Reporter has started.")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/crash", crashReporter)
+	r.HandleFunc("/report/crash", crashReporter)
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("0.0.0.0:5415", nil))
 }
 
 type Crash struct {
-	PlatformArch 	 string `schema:"platformArch,required"`
-	PlatformOs 		 string `schema:"platformOs,required"`
-	SystemCpusNumber int	`schema:"systemCpusNumber,required"`
-    SystemCpus 		 string	`schema:"systemCpus,required"`
-    SystemMemory	 int	`schema:"systemMemory,required"`
-	Timestamp 		 int	`schema:"timestamp,required"`
-	Crash 			 string	`schema:"crash,required"`
+	PlatformArch     string `schema:"platformArch,required"`
+	PlatformOs       string `schema:"platformOs,required"`
+	SystemCpusNumber string `schema:"systemCpusNumber,required"`
+	SystemCpus       string `schema:"systemCpus,required"`
+	SystemMemory     string `schema:"systemMemory,required"`
+	Timestamp        string `schema:"timestamp,required"`
+	Crash            string `schema:"crash,required"`
 }
 
 func crashReporter(w http.ResponseWriter, r *http.Request) {
@@ -48,12 +48,12 @@ func crashReporter(w http.ResponseWriter, r *http.Request) {
 	buf, _ := json.Marshal(c)
 
 	crashID := uuid.Must(uuid.NewRandom())
-	err := ioutil.WriteFile("./crashes/" + crashID.String() + ".json", []byte(buf), 0755)
-	err = ioutil.WriteFile("./crashes/" + crashID.String() + ".txt", []byte(c.Crash), 0755)
-    if err != nil {
-        log.Printf("Unable to write file: %v", err)
+	err := ioutil.WriteFile("./crashes/"+crashID.String()+".json", []byte(buf), 0755)
+	err = ioutil.WriteFile("./crashes/"+crashID.String()+".txt", []byte(c.Crash), 0755)
+	if err != nil {
+		log.Printf("Unable to write file: %v", err)
 	}
-	
+
 	log.Println("New crash report!", crashID.String())
 
 	w.WriteHeader(http.StatusOK)
